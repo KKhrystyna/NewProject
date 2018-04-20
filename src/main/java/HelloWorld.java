048b35803460db9fc1eff518d6886090a9ac5d82
KKhrystyna <khrystynavolosatska@gmaail.com>
2018-04-20 14:51:11 +0300

    Deleted methods 'open' and 'close'. Added method 'read' xlsx

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,38 +1,61 @@
+import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.nio.file.Paths;
-import java.util.Scanner;
+import java.text.SimpleDateFormat;
+import java.util.Iterator;
 
 public class HelloWorld {
-    private static Scanner input;
 
     public static void main(String[] args) {
 
         System.out.println("Hello World!");
 
-        XSSFWorkbook workbook = new XSSFWorkbook();
-
-
-        openFile();
-        closeFile();
+        readFile();
     }
 
-    public static void openFile() {
+    public static void readFile() {
+
         try {
-            input = new Scanner(Paths.get("expenses.xlsx"));
-            while (input.hasNextLine()){
-                String s = input.nextLine();
-                System.out.println(s);
-            }
-        } catch (IOException ioException) {
-            System.err.println("Error opening file. Terminating.");
-            System.exit(1);
-        }
-    }
 
-    public static void closeFile() {
-        if (input != null)
-            input.close();
+            FileInputStream excelFile = new FileInputStream(new File("expenses.xlsx"));
+            Workbook workbook = new XSSFWorkbook(excelFile);
+            Sheet datatypeSheet = workbook.getSheetAt(0);
+            Iterator<Row> iterator = datatypeSheet.iterator();
+
+            while (iterator.hasNext()) {
+
+                Row currentRow = iterator.next();
+                Iterator<Cell> cellIterator = currentRow.iterator();
+
+                while (cellIterator.hasNext()) {
+
+                    Cell currentCell = cellIterator.next();
+
+                    if (currentCell.getCellTypeEnum() == CellType.STRING) {
+                        System.out.printf("%s%s", currentCell.getStringCellValue(), " -- ");
+                    } else if (currentCell.getCellTypeEnum() == CellType.NUMERIC) {
+                        if (DateUtil.isCellDateFormatted(currentCell)) {
+                            System.out.println();
+                            SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
+                            String date = sdf.format(currentCell.getDateCellValue());
+                            System.out.print(date);
+                            System.out.println();
+                        } else {
+                            System.out.printf("%s%s", currentCell.getNumericCellValue(), " --");
+                        }
+                    }
+                }
+                System.out.println();
+
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
